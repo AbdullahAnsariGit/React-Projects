f2e20471e8527fd1c7138ae0c7edffa513164876
Abdullah Ansari <92114680+abdullahtech11@users.noreply.github.com>
2022-01-21 04:22:33 +0500

    Update App.js
    add names

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World by Abdullah Ansari
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sysborg.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Sir Inzamam Malik
         </a>
       </header>
     </div>
